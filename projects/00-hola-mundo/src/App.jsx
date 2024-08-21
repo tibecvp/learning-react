b824b11c5d292ecbdec707701733659d7f3be6d0
Tibe Calderon <tibecvp@gmail.com>
2024-08-20 22:30:09 -0400

    Render components from an array and example of single component

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,8 +1,48 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName : 'tibecvp',
+        name : 'Tiberio Calderon',
+        isFollowing: true
+    },
+    {
+        userName : 'camigomezdev',
+        name : 'Cami Gomez',
+        isFollowing: false
+    },
+    {
+        userName : 'midudev',
+        name : 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName : 'denniscava',
+        name : 'Dennis Calderon',
+        isFollowing: false
+    }
+]
+
 export function App () {
     return (
+        <section className='cardsContainer'>
+            {
+                users.map(user => {
+                    const { userName, name, isFollowing } = user
+                    return (
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
+/*
         <section className='cardsContainer'>
             <TwitterFollowCard userName="tibecvp" initialIsFollowing>
                 Tibe Calderon
@@ -17,5 +57,6 @@ export function App () {
                 Dennis Calderon
             </TwitterFollowCard>
         </section>
+*/
     )
 }
