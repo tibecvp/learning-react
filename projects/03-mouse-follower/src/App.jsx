e96f4f0e0c38d68c7561cad0044d69a5a237ea51
Tibe Calderon <tibecvp@gmail.com>
2025-02-01 22:15:22 -0500

    Project3: Adds button to activate/deactivate follow mouse

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,9 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('Effect', { enabled })
+  }, [enabled])
 
   return (
-    <h3>Project 3</h3>
+    <>
+      <h3>Project 3</h3>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Deactivate' : 'Activate'} Follow Mouse
+      </button>
+    </>
   )
 }
 
