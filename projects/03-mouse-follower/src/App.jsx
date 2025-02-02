c71819bcee7f7cdebaf18d3310a69af27e838974
Tibe Calderon <tibecvp@gmail.com>
2025-02-01 22:42:51 -0500

    Project3: Addsfunctionality fo show the ball following the mouse pointer and the cleanup of the effect

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -2,18 +2,46 @@ import { useEffect, useState } from "react"
 
 function App() {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
     console.log('Effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('Move', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      console.log('Cleanup', { enabled })
+      window.removeEventListener('pointermove', handleMove)
+    }
   }, [enabled])
 
   return (
-    <>
-      <h3>Project 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}
+      />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Deactivate' : 'Activate'} Follow Mouse
       </button>
-    </>
+    </main>
   )
 }
 
