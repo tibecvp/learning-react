0bd3dc36003e3577e20648a6d298edd3bb1cdfb1
Tibe Calderon <tibecvp@gmail.com>
2025-02-27 23:20:48 -0500

    Adds the UI to render the movies data with a mocked JSON

---
+++ projects/05-movies-searcher/src/components/Movies.jsx
@@ -0,0 +1,31 @@
+function ListOfMovies({ movies }) {
+    return (
+        <ul>
+            {
+                movies.map(movie => (
+                    <li key={movie.id}>
+                        <h3>{movie.title}</h3>
+                        <p>{movie.year}</p>
+                        <img src={movie.poster} alt={movie.title} srcset="" />
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
+
+function NoMoviesResults() {
+    return (
+        <p>No movies found</p>
+    )
+}
+
+export function Movies({ movies }) {
+    const hasMovies = movies.length > 0
+
+    return (
+        hasMovies
+            ? <ListOfMovies movies={movies} />
+            : <NoMoviesResults />
+    )
+}
