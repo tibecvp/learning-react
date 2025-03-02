2c9cc2c966bd7ea537f0939a566d65f159e666b0
Tibe Calderon <tibecvp@gmail.com>
2025-03-02 15:39:11 -0500

    P5: Separates the logic of the data handling in a custom hook

--- projects/05-movies-searcher/src/App.jsx
+++ projects/05-movies-searcher/src/App.jsx
@@ -1,17 +1,9 @@
 import './App.css'
-import responseMovies from './mocks/with-results.json'
-import withoutResults from './mocks/without-results.json'
+import { useMovies } from './hooks/useMovies.js'
 import { Movies } from './components/Movies.jsx'
 
 function App() {
-  const movies = responseMovies.Search
-
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
+  const { movies: mappedMovies } = useMovies()
 
   return (
     <div className='page'>
