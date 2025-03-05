fbe688fd66213a4e69a6ab100abc9bd6bd200668
Tibe Calderon <tibecvp@gmail.com>
2025-03-05 09:28:26 -0500

    P6: Renders the products with title, price and Add to Cart button

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,7 +1,10 @@
+import { products } from './mocks/products.json'
+import { Products } from "./components/Products.jsx"
+
 function App() {
 
   return (
-    <h1>Shopping Cart</h1>
+    <Products products={products} />
   )
 }
 
---
+++ projects/06-shopping-cart/src/components/products.jsx
@@ -0,0 +1,24 @@
+import './Products.css'
+import { AddToCartIcon } from './icons'
+
+export function Products({ products }) {
+    return (
+        <main className='products'>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.thumbnail} alt={product.title} />
+                        <div>
+                            <strong>{product.title}</strong> - ${product.price}
+                        </div>
+                        <div>
+                            <button>
+                                <AddToCartIcon />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+}
