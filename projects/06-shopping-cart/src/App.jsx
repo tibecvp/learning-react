baa3073a6a657917eb12ca47290133c1c3534344
Tibe Calderon <tibecvp@gmail.com>
2025-03-06 09:28:15 -0500

    P6: change the json products data because the previous one had the URL images broken

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,5 +1,5 @@
 import { products } from './mocks/products.json'
-import { Products } from "./components/Products.jsx"
+import { Products } from "./components/MyProducts.jsx"
 
 function App() {
 
--- projects/06-shopping-cart/src/components/MyProducts.jsx
+++ projects/06-shopping-cart/src/components/MyProducts.jsx
@@ -5,7 +5,7 @@ export function Products({ products }) {
     return (
         <main className='products'>
             <ul>
-                {products.map(product => (
+                {products.slice(0, 10).map(product => (
                     <li key={product.id}>
                         <img src={product.thumbnail} alt={product.title} />
                         <div>
