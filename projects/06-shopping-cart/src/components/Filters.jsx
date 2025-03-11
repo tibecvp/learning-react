07d1ce99015b93edc83b033ac519998be47aee6c
Tibe Calderon <tibecvp@gmail.com>
2025-03-10 23:43:34 -0400

    P6: Adds filters with their styles

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,3 +1,5 @@
+import './Filters.css'
+
 export function Filters() {
     return (
         <section className="filters">
@@ -10,6 +12,15 @@ export function Filters() {
                     min="0"
                     max="5000"
                 />
+            </div>
+
+            <div>
+                <label htmlFor="category">Category</label>
+                <select id="category">
+                    <option value="all">All</option>
+                    <option value="laptops">Laptops</option>
+                    <option value="smartphones">Smartphones</option>
+                </select>
             </div>
         </section>
     )
