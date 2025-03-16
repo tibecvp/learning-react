fc63415bb51867a3631d24e30a48e16ef52b09e0
Tibe Calderon <tibecvp@gmail.com>
2025-03-16 14:44:42 -0400

    P6: Adds footer withthe filters  information only for development environment

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,12 +1,13 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Header } from "./components/Header.jsx"
 import { Products } from "./components/Products.jsx"
+import { Footer } from './components/Footer.jsx'
 import { useState } from 'react'
+import { IS_DEVELOPMENT } from './config.js'
 
-function App() {
-  const [products] = useState(initialProducts)
+function useFilters() {
+
   const [filters, setFilters] = useState({
-    searchTerm: '',
     category: 'all',
     minPrice: 0,
   })
@@ -23,12 +24,20 @@ function App() {
     })
   }
 
+  return { filters, filterProducts, setFilters }
+}
+
+function App() {
+  const [products] = useState(initialProducts)
+  const { filters, filterProducts, setFilters } = useFilters()
+
   const filteredProducts = filterProducts(products)
 
   return (
     <>
       <Header changeFilters={setFilters} />
       <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer filters={filters}></Footer>}
     </>
   )
 }
--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
 import './Filters.css'
 
 export function Filters({ onChange }) {
     const [minPrice, setMinPrice] = useState(0)
+    const minPriceFilterId = useId()
+    const categoryFilterId = useId()
 
     const handleChangeMinPrice = (event) => {
         setMinPrice(event.target.value)
@@ -22,10 +24,10 @@ export function Filters({ onChange }) {
     return (
         <section className="filters">
             <div>
-                <label htmlFor="price">Price</label>
+                <label htmlFor={minPriceFilterId}>Price</label>
                 <input
                     type="range"
-                    id="price"
+                    id={minPriceFilterId}
                     name="price"
                     min="0"
                     max="2000"
@@ -35,8 +37,8 @@ export function Filters({ onChange }) {
             </div>
 
             <div>
-                <label htmlFor="category">Category</label>
-                <select id="category" onChange={handleChangeCategory}>
+                <label htmlFor={categoryFilterId}>Category</label>
+                <select id={categoryFilterId} onChange={handleChangeCategory}>
                     <option value="all">All</option>
                     <option value="laptops">Laptops</option>
                     <option value="smartphones">Smartphones</option>
