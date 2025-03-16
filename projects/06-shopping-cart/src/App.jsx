32da65fde76e59c745a4245a7758476dfb0cc60a
Tibe Calderon <tibecvp@gmail.com>
2025-03-16 15:54:12 -0400

    P6: Adds a context and use the useContext of react

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -2,15 +2,17 @@ import { products as initialProducts } from './mocks/products.json'
 import { Header } from "./components/Header.jsx"
 import { Products } from "./components/Products.jsx"
 import { Footer } from './components/Footer.jsx'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { IS_DEVELOPMENT } from './config.js'
+import { FiltersContext } from './context/filters.jsx'
 
 function useFilters() {
 
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0,
-  })
+  // const [filters, setFilters] = useState({
+  //   category: 'all',
+  //   minPrice: 0,
+  // })
+  const { filters, setFilters } = useContext(FiltersContext)
 
   const filterProducts = (products) => {
     return products.filter(product => {
--- projects/06-shopping-cart/src/main.jsx
+++ projects/06-shopping-cart/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { FiltersProvider } from './context/filters.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <FiltersProvider>
     <App />
-  </StrictMode>,
+  </FiltersProvider>
 )
