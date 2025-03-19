8783f70f0902c21aaede3522f5a036c9862ee9c2
Tibe Calderon <tibecvp@gmail.com>
2025-03-18 21:58:37 -0400

    P6: Adds functionality to show the cart items, increase item quantity, and, clear cart

--- projects/06-shopping-cart/src/components/Cart.jsx
+++ projects/06-shopping-cart/src/components/Cart.jsx
@@ -1,9 +1,33 @@
 import { useId } from 'react'
 import './Cart.css'
 import { CartIcon, ClearCartIcon, RemoveFromCartIcon } from './Icons.jsx'
+import { useCart } from '../hooks/useCart.js'
+
+function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+    return (
+        <li>
+            <img
+                src={thumbnail}
+                alt={title}
+            />
+            <div>
+                <strong>{title}</strong> - ${price}
+            </div>
+
+            <footer>
+                <small>
+                    Qty: {quantity}
+                </small>
+                <button onClick={addToCart}>+</button>
+            </footer>
+        </li>
+    )
+}
 
 export function Cart() {
     const cartCheckboxId = useId()
+    const { cart, clearCart, addToCart } = useCart()
+
     return (
         <>
             <label className='cart-button' htmlFor={cartCheckboxId}>
@@ -13,25 +37,16 @@ export function Cart() {
 
             <aside className='cart'>
                 <ul>
-                    <li>
-                        <img
-                            src='https://cdn.dummyjson.com/products/images/smartphones/iPhone%205s/thumbnail.png'
-                            alt='Iphone 5s'
+                    {cart.map(product => (
+                        <CartItem
+                            key={product.id}
+                            addToCart={() => addToCart(product)}
+                            {...product}
                         />
-                        <div>
-                            <strong>iphone 5s</strong> - $199.99
-                        </div>
-
-                        <footer>
-                            <small>
-                                Qty: 1
-                            </small>
-                            <button>+</button>
-                        </footer>
-                    </li>
+                    ))}
                 </ul>
 
-                <button>
+                <button onClick={clearCart}>
                     <ClearCartIcon />
                     Clear Cart
                 </button>
